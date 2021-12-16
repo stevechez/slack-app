6a654a4217b0cb699e83da455218054b68d1ee0e
Steve M <stevem@Steves-MacBook-Air.local>
2021-12-15 22:00:25 -0800

    update sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components'
 import Header from './components/Header'
 import Home from './components/Home'
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <AppBody>
+          <Sidebar>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Sidebar>
+        </AppBody>
+      
 
     </Router>
   );
 }
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`
 
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import CreateIcon from '@material-ui/icons/Create'
+import SidebarOption from './SidebarOption'
+import InsertCommentIcon from '@material-ui/icons/InsertComment'
+import InboxIcon from '@material-ui/icons/Inbox'
+import DraftsIcon from '@material-ui/icons/Drafts'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
+import AppsIcon from '@material-ui/icons/Apps'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
 
 const Sidebar = () => {
     return (
@@ -8,23 +18,69 @@ const Sidebar = () => {
             <SidebarHeader>
                 <SidebarInfo>
                     <h2>SlackMe</h2>
-                    <h3><FiberManualRecordIcon /></h3>
+                    <h3><FiberManualRecordIcon />
+                    Steve Chez
+                    </h3>
                 </SidebarInfo>
+                <CreateIcon />
             </SidebarHeader>
+            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People & User Groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
             
         </SidebarContainer>
     )
 }
 
 const SidebarContainer = styled.div`
-
+    background-color: purple;
+    color: #fff;
+    flex: 0.3;
+    border-top: 1px solid #49274b;
+    max-width: 260px;
+    margin-top: 60px;
 `
 
 const SidebarHeader = styled.div`
+    display: flex;
+    border-bottom: #49274b;
+    padding: 13px;
 
+    > MuiSvgIcon-root {
+        padding: 8px;
+        border-bottom: #49274b;
+        font-size: 18px;
+        background-color: #fff;
+        border-radius: 999px;
+
+    }
 `
 
 const SidebarInfo = styled.div`
+    flex: 1;
+    > h2 {
+        font-size: 15px;
+        font-weight: 900;
+        margin-bottom: 5px;
+    }
+    > h3 {
+        display: flex;
+        font-size: 13px;
+        font-weight: 400;
+        align-items: center;
+
+        > .MuiSvgIcon-root {
+            font-size: 14px;
+            margin-top: 1px;
+            margin-right: 2px;
+            color: green;
+        }
+    }
 `
 
 export default Sidebar
